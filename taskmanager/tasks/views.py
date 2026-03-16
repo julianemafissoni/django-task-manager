7cee899f5a57e5a3c5a51abc386fef2b2c64905f
julianemafissoni <julianemafisoni@hotmail.com>
2026-03-16 13:41:00 -0300

    pagination, ordering and documentation. final adjustments of the project

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -10,7 +10,9 @@ class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
 
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    
+    ordering_fields = ['created_art', 'status', 'title']
     filterset_fields = ['status']
     search_fields = ['title', 'description']
 
